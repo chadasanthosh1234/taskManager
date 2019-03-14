d11ea042323f7854c4e3b1726e1cfa1b522181a3
chada <santhoshkumar.chada@cognizant.com>
2019-03-14 07:56:59 +0530

    Updating frontend changes after build

--- src/main/resources/static/styles.js
+++ src/main/resources/static/styles.js
@@ -557,7 +557,7 @@ if(false) {}
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\Admin\Downloads\taskManager\taskManager\frontend\src\styles.css */"./src/styles.css");
+module.exports = __webpack_require__(/*! C:\taskManagerGitWorkspace\frontend\src\styles.css */"./src/styles.css");
 
 
 /***/ })
