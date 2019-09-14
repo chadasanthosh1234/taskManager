18ecdebf29b0c6a61ec304f71a256fc17b9625e1
chada <santhoshkumar.chada@cognizant.com>
2019-09-14 21:56:48 +0530

    final certification 2

--- src/main/java/com/cts/taskManager/controllers/ProjectController.java
+++ src/main/java/com/cts/taskManager/controllers/ProjectController.java
@@ -1,5 +1,44 @@
 package com.cts.taskManager.controllers;
 
-public class ProjectController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.cts.taskManager.repositories.ProjectRepository;
+import com.cts.taskManager.util.Project;
+
+@RestController
+public class ProjectController {
+	@Autowired
+    ProjectRepository projectRepository;
+	
+	@RequestMapping(method=RequestMethod.GET, value="/projects")
+    public Iterable<Project> projects() {
+        return projectRepository.findAllProjects();
+    }
+
+	@RequestMapping(method=RequestMethod.POST, value="/projects")
+    public Project saveProject(@RequestBody Project project) {
+        projectRepository.addProject(project);
+        return project;
+    }
+	
+	@RequestMapping(method=RequestMethod.GET, value="/projects/{id}")
+    public Project showProject(@PathVariable String projectId) {
+        return projectRepository.findById(projectId);
+    }
+	
+	@RequestMapping(method=RequestMethod.POST, value="/projects/{id}")
+    public Project updateUser(@PathVariable String projectId, @RequestBody Project project) {
+        Project c = projectRepository.findById(projectId);
+        if(project.getProjectName() != null)
+            c.setProjectName(project.getProjectName());
+       
+        projectRepository.addProject(c);
+        return project;
+    }
+	
 }
--- src/main/java/com/cts/taskManager/controllers/UserController.java
+++ src/main/java/com/cts/taskManager/controllers/UserController.java
@@ -1,5 +1,51 @@
 package com.cts.taskManager.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cts.taskManager.repositories.UserRepository;
+import com.cts.taskManager.util.User;
+
+
+@RestController
 public class UserController {
 
+	@Autowired
+    UserRepository userRepository;
+	
+	@RequestMapping(method=RequestMethod.GET, value="/users")
+    public Iterable<User> users() {
+        return userRepository.findAllUsers();
+    }
+
+	@RequestMapping(method=RequestMethod.POST, value="/users")
+    public User saveUser(@RequestBody User user) {
+        userRepository.addUser(user);
+        return user;
+    }
+	
+	@RequestMapping(method=RequestMethod.GET, value="/users/{id}")
+    public User showUser(@PathVariable String employeeId) {
+        return userRepository.findById(employeeId);
+    }
+	
+	@RequestMapping(method=RequestMethod.POST, value="/users/{id}")
+    public User updateUser(@PathVariable String employeeId, @RequestBody User user) {
+        User c = userRepository.findById(employeeId);
+        if(user.getFirstName() != null)
+            c.setFirstName(user.getFirstName());
+        if(user.getLastName() != null)
+            c.setLastName(user.getLastName());
+        if(user.getProjectId() != null)
+            c.setProjectId(user.getProjectId());
+        if(user.getTaskId() != null)
+            c.setTaskId(user.getTaskId());
+        userRepository.addUser(c);
+        return user;
+    }
+	
 }
